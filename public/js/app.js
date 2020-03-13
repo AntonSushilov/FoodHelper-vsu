dde54ea581ac067fbc7b3acecd5bb6d46b77a2fd
Anton <39156677+AntonSushilov@users.noreply.github.com>
2020-03-13 17:28:25 +0300

    fin

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\xampp\htdocs\FoodHelper\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\xampp\htdocs\FoodHelper\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\xampp\htdocs\blog\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\xampp\htdocs\blog\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -2,26 +2,6 @@
 @section('content')
 <div class="container"> <h1>Форма</h1></div>
 	
-	<form action="/add/submit" method="post">
-		<div class="form-group">
-			<label for="name">Введите имя</label>
-			<input type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
-		</div>
-
-		<div class="form-group">
-			<label for="email">Введите email</label>
-			<input type="text" name="name" placeholder="Введите email" id="email" class="form-control">
-		</div>
-
-		<div class="form-group">
-			<label for="subject">Тема сообщения</label>
-			<input type="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
-		</div>
-
-		<div class="form-group">
-			<label for="message">Тема сообщения</label>
-			<textarea name="message" id="message" class="form-control"></textarea>
-		</div>
-	</form>
+	
 
 @endsection
