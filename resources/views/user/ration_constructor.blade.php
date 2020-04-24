4578c4bdc3c41b12a0e9d4694f7b39022f2611bc
Sotnikov-Vladimir <39740656+Sotnikov-Vladimir@users.noreply.github.com>
2020-04-24 14:06:56 +0300

    доделал

--- resources/views/user/ration_constructor.blade.php
+++ resources/views/user/ration_constructor.blade.php
@@ -7,19 +7,19 @@
    <div class="container">
        <div class="both7 parent">
            <div class="left">
-               <div class="prod jumbotron" id="q1">
+               <div class="prod jumbotron" id="divLeft">
                     <div class="both7 up">
                        <h3>Продукты</h3>
                        <input class="find" type="text" id="myInputId" onkeyup="searchId()" placeholder="Поиск по продуктам">
                     </div>
-                    <div class="cardss" >
+                    <div class="cardss" id="divLeft1">
                         <div class="card" draggable="true" ondragstart="drag(event)" id="drag1">
                             <img class="card-image" src="../../public/img/dim.jpg" alt="Фото продукта">
                             <div class="card-text" id="mydivheader">
                                 <p >котлета</p>
                             </div>
                         </div>
-                        <div class="card" raggable="true" ondragstart="drag(event)" id="drag2" >
+                        <div class="card" draggable="true" ondragstart="drag(event)" id="drag2" >
                             <img class="card-image" src="../../public/img/dim.jpg" alt="Фото продукта">
                             <div class="card-text" id="mydivheader">
                                 <p >котлета</p>
@@ -29,13 +29,13 @@
                     </div>
 
                </div>
-               <div class="prod jumbotron">
+               <div class="prod jumbotron" id="divLeft">
                    <div class="both7 up">
                        <h3>Блюда</h3>
                        <input class="find" type="text" id="myInputId" onkeyup="searchId()" placeholder="Поиск по блюдам">
                    </div>
-                   <div class="cardss" id='draggableSpan2' draggable='true' ondragstart='onDragStart(event);'>
-                        <div class="card">
+                   <div class="cardss" id="divLeft1">
+                        <div class="card" draggable="true" ondragstart="drag(event)" id="drag1">
                             <img class="card-image" src="../../public/img/dim.jpg" alt="Фото продукта">
                             <div class="card-text">
                                 <p >Котлеты</p>
@@ -44,11 +44,11 @@
                     </div>
                </div>
            </div>
-           <div class="right">
+           <div class="right" id="divRight">
                 <div class="day jumbotron" >
                   <h3>Завтрак</h3>
                   <form action="" >
-                     <div class="zone" ondrop="drop(event)" ondragover="allowDrop(event)">
+                     <div class="zone" id="divRight1" ondrop="drop(event)" ondragover="allowDrop(event)">
 
                      </div>
                   </form>
@@ -56,7 +56,7 @@
                 <div class="day jumbotron">
                   <h3>Обед</h3>
                   <form action="">
-                     <div class="zone" ondrop="drop(event)" ondragover="allowDrop(event)">
+                     <div class="zone" id="divRight2" ondrop="drop(event)" ondragover="allowDrop(event)">
 
                      </div>
                   </form>
@@ -64,7 +64,7 @@
                 <div class="day jumbotron">
                   <h3>Ужин</h3>
                   <form action="">
-                     <div class="zone" ondrop="drop(event)" ondragover="allowDrop(event)">
+                     <div class="zone" id="divRight3" ondrop="drop(event)" ondragover="allowDrop(event)">
 
                      </div>
                   </form>
@@ -75,62 +75,22 @@
 
 
 <script>
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
 
-
-    function allowDrop(ev) {
-        ev.preventDefault();
-    }
-
-    function drag(ev) {
-        ev.dataTransfer.setData("text", ev.target.id);
-    }
-
-    function drop(ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
-        var nodeCopy = document.getElementById(data).cloneNode(true);
-        ev.target.appendChild(nodeCopy);
-        ev.stopPropagation();
-        return false;
-    }
-   /* const firstDiv = document.querySelector( ".card" );
-
-
-
-    function onDragStart(event) {
-        event
-        .dataTransfer
-        .setData('text/plain', event.target.id);
-
-        event
-        .currentTarget
-        .style
-        .backgroundColor = '';
-    }
-
-    function onDragOver(event) {
-        event.preventDefault();
-    }
-
-    function onDrop(event) {
-        const id = event
-        .dataTransfer
-        .getData('text');
-
-        const draggableElement = document.getElementById(id);
-        const dropzone = event.target;
-
-        dropzone.appendChild(draggableElement);
-        const newDiv = firstDiv.cloneNode( true ); // клонируем элемент с его потомками
-        document.p.appendChild( newDiv ); // добавляем клонированный элемент элементу <body>
-
-
-
-        event
-        .dataTransfer
-        .clearData();
-    }*/
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var nodeCopy = document.getElementById(data).cloneNode(true);
+    ev.target.appendChild(nodeCopy);
+    ev.stopPropagation();
+    return false;
+}
 </script>
 
 
